01d1e00b08fba7f7024866bd721979d270202942
Grant Montgomery <lettucebowler@gmail.com>
2025-02-12 01:41:10 -0600

    try wrapping in arrow function

--- apps/wordlettuce/src/lib/auth.server.ts
+++ apps/wordlettuce/src/lib/auth.server.ts
@@ -9,7 +9,7 @@ function _createAuthClient(event: RequestEvent) {
     clientID: 'lettuce-auth-test',
     issuer: AUTH_HOST,
     // fetch: event.fetch,
-    fetch: dev ? event.fetch : event.platform!.env!.lettuce_auth!.fetch.bind(globalThis),
+    fetch: dev ? event.fetch : (a, b) => event.platform!.env!.lettuce_auth!.fetch(a, b),
     // fetch: event.platform?.env.lettuce_auth?.fetch ?? event.fetch,
     // fetch: event.platform?.env?.lettuce_auth.fetch ? event.platform.env.lettuce_auth.fetch : event.fetch,
     storage: event.platform?.env?.lettuce_auth_signing_keys,
