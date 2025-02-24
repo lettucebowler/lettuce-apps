3b44c952905758525c4692d6165d5d1d68b7a7db
Grant Montgomery <lettucebowler@gmail.com>
2025-02-24 17:22:17 -0600

    wip

--- apps/api-wordlettuce-fly/drizzle.config.ts
+++ apps/api-wordlettuce-fly/drizzle.config.ts
@@ -4,11 +4,9 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   out: './drizzle',
   schema: './drizzle/schema.ts',
-  dialect: 'sqlite',
-  driver: 'd1-http',
+  dialect: 'turso',
   dbCredentials: {
-    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-    token: process.env.CLOUDFLARE_D1_TOKEN!,
+    url: process.env.TURSO_CONNECTION_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 });
