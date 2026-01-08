fe82cf255b8c98639c243f3608d0762e5bbb2a11
Grant Montgomery <lettucebowler@gmail.com>
2026-01-07 18:06:42 -0600

    update deps

--- apps/lettuce-bookshelf/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import devtoolsJson from 'vite-plugin-devtools-json';
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import ViteYAML from '@modyfi/vite-plugin-yaml';
-
-export default defineConfig({ plugins: [tailwindcss(), sveltekit(), devtoolsJson(), ViteYAML()] });
