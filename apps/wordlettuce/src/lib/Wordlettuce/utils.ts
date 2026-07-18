0fbd506bd95d8af97b81126ca23704e8b42c42be
Grant Montgomery <lettucebowler@gmail.com>
2026-07-17 20:50:42 -0500

    oops

--- apps/wordlettuce/src/lib/Wordlettuce/utils.ts
+++ apps/wordlettuce/src/lib/Wordlettuce/utils.ts
@@ -5,7 +5,6 @@ import { appName } from '$lib/app-constants';
 const msInADay = 1000 * 60 * 60 * 24;
 const initial = new Date(1643673600000);
 export function getGameNum() {
-  return 1627;
   const now = new Date();
   return Math.floor((now.getTime() - initial.getTime()) / msInADay);
 }
